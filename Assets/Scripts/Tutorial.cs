b2b078c30bb0b362f2013ef62cb626f8c32a9771
Laurie McCulloch <laurie.mcculloch@ntlworld.com>
2020-03-22 21:38:56 +0000

    LevelUp event Added

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -1,17 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 using DeltaDNA; 
 
 public class Tutorial : MonoBehaviour
 {
+    int userLevel = 1;
+    public Text txtUserLevel ; 
+
     // Start is called before the first frame update
     void Start()
     {
+        txtUserLevel.text = string.Format("Level : {0}", userLevel);
+
+
         DDNA.Instance.SetLoggingLevel(DeltaDNA.Logger.Level.DEBUG);
         DDNA.Instance.StartSDK();
 
     }
 
+    public void BttnLevelUp_Clicked()
+    {
+        userLevel++;
+        txtUserLevel.text = string.Format("Level : {0}" , userLevel);
+
+
+        GameEvent myEvent = new GameEvent("levelUp")
+            .AddParam("userLevel", userLevel)
+            .AddParam("levelUpName", string.Format("Level {0}", userLevel));
+
+        DDNA.Instance.RecordEvent(myEvent).Run();
+    }
 
 }
